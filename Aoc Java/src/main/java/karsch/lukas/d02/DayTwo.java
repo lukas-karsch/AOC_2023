b0876018b0bcea01be08357095d99a14f7548677
Lukas Karsch <lukas.karsch03@gmail.com>
2023-12-09 10:41:14 +0100

    day 2 part 2

--- Aoc Java/src/main/java/karsch/lukas/d02/DayTwo.java
+++ Aoc Java/src/main/java/karsch/lukas/d02/DayTwo.java
@@ -7,40 +7,69 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class DayTwo implements AocSolver {
-    private final Stream<String> input;
+    private final List<String> input;
 
     private final static int RED_CUBES = 12;
     private final static int GREEN_CUBES = 13;
     private final static int BLUE_CUBES = 14;
 
     public DayTwo(Stream<String> input) {
-        this.input = input;
+        this.input = input.toList();
     }
 
     public void solve() {
-        int sum = input.map(line -> {
-            int gameId = Integer.parseInt(line.split(":")[0].split(" ")[1].trim());
-            var isGameValid = Arrays.stream(line.split(":")[1].split(";")).allMatch(
-                    input -> {
-                        System.out.print(input);
-                        var split = input.split(",");
-                        for (String s : split) {
-                            String[] amountAndColor = s.trim().split(" ");
-                            if (amountAndColor[1].trim().equals("blue")) {
-                                if (Integer.parseInt(amountAndColor[0]) > BLUE_CUBES) return false;
-                            } else if (amountAndColor[1].trim().equals("red")) {
-                                if (Integer.parseInt(amountAndColor[0]) > RED_CUBES) return false;
-                            } else {
-                                if (Integer.parseInt(amountAndColor[0]) > GREEN_CUBES) return false;
+        partOne(input);
+        partTwo(input);
+    }
+
+    private void partOne(List<String> input) {
+        int sum = input.stream()
+                .map(line -> {
+                    int gameId = Integer.parseInt(line.split(":")[0].split(" ")[1].trim());
+                    var isGameValid = Arrays.stream(line.split(":")[1].split(";")).allMatch(
+                            gameSection -> {
+                                var split = gameSection.split(",");
+                                for (String s : split) {
+                                    String[] amountAndColor = s.trim().split(" ");
+                                    if (amountAndColor[1].trim().equals("blue")) {
+                                        if (Integer.parseInt(amountAndColor[0]) > BLUE_CUBES) return false;
+                                    } else if (amountAndColor[1].trim().equals("red")) {
+                                        if (Integer.parseInt(amountAndColor[0]) > RED_CUBES) return false;
+                                    } else {
+                                        if (Integer.parseInt(amountAndColor[0]) > GREEN_CUBES) return false;
+                                    }
+                                }
+                                return true;
                             }
-                        }
-                        return true;
-                    }
-            );
-            System.out.println(line + "   is valid: " + isGameValid);
-            if(isGameValid) return gameId;
-            return 0;
-        }).reduce(0, Integer::sum);
+                    );
+                    if (isGameValid) return gameId;
+                    return 0;
+                }).reduce(0, Integer::sum);
         System.out.println("Sum of valid game IDs is " + sum);
+    }
+
+    private void partTwo(List<String> input) {
+        int sumOfPowers = input.stream().
+                map(line -> {
+                    String gameContent = line.split(":")[1].trim();
+                    final int[] cubes = new int[3];
+                    Arrays.stream(gameContent.split(";")).forEach(
+                            section -> {
+                                var split = section.split(",");
+                                for (String s : split) {
+                                    String[] amountAndColor = s.trim().split(" ");
+                                    if (amountAndColor[1].trim().equals("red")) {
+                                        cubes[0] = Math.max(cubes[0], Integer.parseInt(amountAndColor[0]));
+                                    } else if (amountAndColor[1].trim().equals("green")) {
+                                        cubes[1] = Math.max(cubes[1], Integer.parseInt(amountAndColor[0]));
+                                    } else {
+                                        cubes[2] = Math.max(cubes[2], Integer.parseInt(amountAndColor[0]));
+                                    }
+                                }
+                            });
+                    return cubes[0] * cubes[1] * cubes[2];
+                })
+                .reduce(0, Integer::sum);
+        System.out.println("Sum of powers is: " + sumOfPowers);
     }
 }
