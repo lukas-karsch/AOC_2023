8c3d1d3a28805636a95e4eacfa74a950fe8542c4
Lukas Karsch <lukas.karsch03@gmail.com>
2023-12-09 09:21:33 +0100

    day 1 part 2

--- 01/01.js
+++ 01/01.js
@@ -8,32 +8,85 @@ function getFirstDigit(line) {
     for(let i=0; i<line.length; i++) {
         const char = line.charAt(i);
         if(Number.isInteger(parseInt(char))) {
-            //console.log(char, "is integer");
-            return char;
+            return {index: i, char: char};
         }
     }
-    return null;
+    return {index: -1}
+}
+
+/**
+ * @param {string} line 
+ */
+function getFirstNamedDigit(line) {
+    for (let i = 0; i < line.length - 3; i++) {
+        const sub = line.substring(i, Math.min(i+5, line.length));
+        const NamedDigit = isNamedDigit(sub);
+        if(NamedDigit) {
+            return {index: i, digit: NamedDigit};
+        }
+    }
+    return {index: -1}
+}
+
+/**
+ * @param {string} line 
+ */
+function getLastNamedDigit(line) {
+    for (let i = line.length - 3; i > 0; i--) {
+        const sub = line.substring(i, Math.min(i+5, line.length));
+        const NamedDigit = isNamedDigit(sub);
+        if(NamedDigit) {
+            return {index: i, digit: NamedDigit};
+        }
+    }
+    return {index: -1}
 }
 
 function getLastDigit(line) {
     for(let i=line.length; i>=0; i--) {
         const char = line.charAt(i);
         if(Number.isInteger(parseInt(char))) {
-            //console.log(char, "is integer");
-            return char;
+            return {index: i, char: char};
         }
     }
-    return null;
+    return {index: -1}
+}
+
+/**
+ * @param {string} input 
+ */
+function isNamedDigit(input) {
+    if(input.startsWith("one")) return 1;
+    if(input.startsWith("two")) return 2;
+    if(input.startsWith("three")) return 3;
+    if(input.startsWith("four")) return 4;
+    if(input.startsWith("five")) return 5;
+    if(input.startsWith("six")) return 6;
+    if(input.startsWith("seven")) return 7;
+    if(input.startsWith("eight")) return 8;
+    if(input.startsWith("nine")) return 9;
 }
 
 function getLineDigits(line) {
     let result = "";
-    result += getFirstDigit(line);
-    result += getLastDigit(line);
-    console.log(result);
-    if(result.length !== 2) {
-        return 0;
+    let first = "";
+    let second = "";
+    const firstNamedDigit = getFirstNamedDigit(line);
+    const firstDigit = getFirstDigit(line);
+    if(firstNamedDigit.index === -1) first += firstDigit.char;
+    else {
+        if(firstDigit.index < firstNamedDigit.index) first += firstDigit.char;
+        else first += firstNamedDigit.digit;
     }
+    const lastNamedDigit = getLastNamedDigit(line);
+    const lastDigit = getLastDigit(line);
+    if(lastNamedDigit.index === -1) second += lastDigit.char;
+    else {
+        if(lastDigit.index > lastNamedDigit.index) second += lastDigit.char;
+        else second += lastNamedDigit.digit;
+    }
+    result = first + second;
+    console.log(line, result);
     return result;
 }
 
