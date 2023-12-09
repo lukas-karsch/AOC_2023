66408caa4ff23a8989ea625733978be47ca0658f
Lukas Karsch <lukas.karsch03@gmail.com>
2023-12-09 19:12:22 +0100

    day 5 skeleton

---
+++ Aoc Java/src/main/java/karsch/lukas/solutions/DayFive.java
@@ -0,0 +1,19 @@
+package karsch.lukas.solutions;
+
+import karsch.lukas.AocSolver;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DayFive implements AocSolver {
+    private final List<String> input;
+
+    public DayFive(Stream<String> input) {
+        this.input = input.toList();
+    }
+
+    @Override
+    public void solve() {
+
+    }
+}
